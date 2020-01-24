e77c24b02da6e805320a3a169a4a471a490a32a6
theNerd247 <noah.harvey247 gm>
2020-01-23 21:06:56 -0500

    adds existential types package

--- existential.ts
+++ existential.ts
@@ -1,16 +1,45 @@
-import { HKT, HKT2, Kind2, URIS2 } from 'fp-ts/lib/HKT'
+import { HKT2, Kind2, URIS2 } from 'fp-ts/lib/HKT'
 
-const ExtURI = "Existential"
-type ExtURI = typeof ExtURI
+const URI = "Existential"
+type URI = typeof URI
 
-export interface Existential<F extends URIS2, A> {
-  readonly URI: F
-  <R>(cont : <E>(t : Kind2<F,A,E>) => R) : R
+export interface Existential<F extends URIS2, A>
+  { <R>(cont : <E>(t : Kind2<F,A,E>) => R) : R
+  }
+  
+const liftExt = <F extends URIS2,A,E>(x : Kind2<F,A,E>) : Existential<F,A> =>
+  <R>(cont : <X>(t : Kind2<F,A,X>) => R) => cont(x) 
+
+interface FooE<A,E> {
+  _tag : "FooE"
+  a    : A
+  ex   : E
 }
 
-//const mkExistential = <F,A,E>(x : HKT2<F,A,E>) : Existential<F,A> =>
-//  <R>(cont : <E>(t : HKT2<F,A,E>) => R) => cont(x)
+const fooE_ = <A,E>(a : A, ex : E) : FooE<A,E> => (
+  { _tag : "FooE" 
+  , a 
+  , ex
+  }
+)
 
 declare module 'fp-ts/lib/HKT' {
-  
+  interface URItoKind2<E,A> {
+    FooE: FooE<E,A>
+  }
 }
+
+const FooURI = "FooE"
+type FooURI = typeof FooURI
+
+type Foo<A> = Existential<FooURI,A>
+
+const foo = <A,E>(a : A, ex : E) : Foo<A> =>
+  liftExt<FooURI,A,E>(fooE_(a, ex))
+
+const foos : Foo<string>[] = 
+  [ foo("boo", 2)
+  , foo("bar", true)
+  ]
+
+
