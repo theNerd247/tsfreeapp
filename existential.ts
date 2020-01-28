fea3054aef9c1bc40dffed9ed3d419b4fff4091b
theNerd247 <noah.harvey247 gm>
2020-01-28 10:56:13 -0500

    finished mapExt!

--- existential.ts
+++ existential.ts
@@ -20,10 +20,6 @@ export interface Existential4<F extends URIS4, A, B, C>
   }
   
   
-//export function liftExt<F extends URIS4,A,B,C,E>(x : Kind4<F,A,B,C,E>) : <R>(cont : <X>(t : Kind4<F,A,B,C,X>) => R) => Existential4<F,A,B,C> 
-//export function liftExt<F extends URIS3,A,B,E>(x : Kind3<F,A,B,E>) : <R>(cont : <X>(t : Kind3<F,A,B,X>) => R) => Existential3<F,A,B>
-//export function liftExt<F extends URIS2,A,E>(x : Kind2<F,A,E>) : <R>(cont : <X>(t : Kind2<F,A,X>) => R) => Existential2<F,A> 
-//export function liftExt<F extends URIS,E>(x : Kind<F,E>) : <R>(cont : <X>(t : Kind<F,X>) => R) => Existential<F> {
 export function liftExt<F extends URIS4,A,B,C,E>(x : Kind4<F,A,B,C,E>) : Existential4<F,A,B,C> 
 export function liftExt<F extends URIS3,A,B,E>(x : Kind3<F,A,B,E>) : Existential3<F,A,B>
 export function liftExt<F extends URIS2,A,E>(x : Kind2<F,A,E>) : Existential2<F,A> 
@@ -31,9 +27,9 @@ export function liftExt<F extends URIS,E>(x : Kind<F,E>) : Existential<F> {
   return { runExt : (cont) => cont(x) }
 }
 
-//export function mapExt<F extends URIS4, A, B, C, AA, BB, CC>(f : <E>(a : Kind4<F,A, B, C, E>) => Kind4<F, AA, BB, CC, E>) : (x : Existential4<F, A, B, C>) => Existential4<F,AA, BB, CC> 
-//export function mapExt<F extends URIS3, A, B, AA, BB>(f : <E>(a : Kind3<F,A, B, E>) => Kind3<F,AA, BB, E>) : (x : Existential3<F,A,B>) => Existential3<F,AA, BB>
-//export function mapExt<F extends URIS2, A, B>(f : <E>(a : Kind2<F,A,E>) => Kind2<F,B,E>, x : Existential2<F,A>) : Existential2<F,B>
-//export function mapExt<F extends URIS>(f : <E>(a : Kind<F,E>) => Kind<F,E>, x : Existential<F>) : Existential<F> {
-//  return liftExt(x(f))
-//}
+export function mapExt<F extends URIS4, A, B, C, AA, BB, CC>(f : <E>(a : Kind4<F, A, B, C, E>) => Kind4<F, AA, BB, CC, E>, x : Existential4<F, A, B, C>) : Existential4<F, AA, BB, CC>
+export function mapExt<F extends URIS3, A, B, AA, BB>(f : <E>(a : Kind3<F, A, B, E>) => Kind3<F, AA, BB, E>, x : Existential3<F, A, B>) : Existential3<F, AA, BB>
+export function mapExt<F extends URIS2, A, B>(f : <E>(a : Kind2<F,A,E>) => Kind2<F,B,E>, x : Existential2<F,A>) : Existential2<F,B>
+export function mapExt<F extends URIS>(f : <E>(a : Kind<F,E>) => Kind<F,E>, x : Existential<F>) : Existential<F> {
+  return liftExt(x.runExt(f))
+}
