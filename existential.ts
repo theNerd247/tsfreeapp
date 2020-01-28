b2aedcd1d4186cadba2de83c0663317d76b1c769
theNerd247 <noah.harvey247 gm>
2020-01-28 10:52:21 -0500

    changes Existential quantified types to use a reified structure

--- existential.ts
+++ existential.ts
@@ -1,45 +1,39 @@
-import { HKT2, Kind2, URIS2 } from 'fp-ts/lib/HKT'
+import { Kind, Kind2, Kind3, Kind4, URIS, URIS2, URIS3, URIS4 } from 'fp-ts/lib/HKT'
 
 const URI = "Existential"
 type URI = typeof URI
 
-export interface Existential<F extends URIS2, A>
-  { <R>(cont : <E>(t : Kind2<F,A,E>) => R) : R
+export interface Existential<F extends URIS>
+  { runExt : <R>(cont : <E>(t : Kind<F, E>) => R) => R
   }
   
-const liftExt = <F extends URIS2,A,E>(x : Kind2<F,A,E>) : Existential<F,A> =>
-  <R>(cont : <X>(t : Kind2<F,A,X>) => R) => cont(x) 
+export interface Existential2<F extends URIS2, A>
+  { runExt : <R>(cont : <E>(t : Kind2<F, A, E>) => R) => R
+  }
 
-interface FooE<A,E> {
-  _tag : "FooE"
-  a    : A
-  ex   : E
+export interface Existential3<F extends URIS3, A, B>
+  { runExt : <R>(cont : <E>(t : Kind3<F,A, B, E>) => R) => R
+  }
+  
+export interface Existential4<F extends URIS4, A, B, C>
+  { runExt : <R>(cont : <E>(t : Kind4<F,A, B, C, E>) => R) => R
+  }
+  
+  
+//export function liftExt<F extends URIS4,A,B,C,E>(x : Kind4<F,A,B,C,E>) : <R>(cont : <X>(t : Kind4<F,A,B,C,X>) => R) => Existential4<F,A,B,C> 
+//export function liftExt<F extends URIS3,A,B,E>(x : Kind3<F,A,B,E>) : <R>(cont : <X>(t : Kind3<F,A,B,X>) => R) => Existential3<F,A,B>
+//export function liftExt<F extends URIS2,A,E>(x : Kind2<F,A,E>) : <R>(cont : <X>(t : Kind2<F,A,X>) => R) => Existential2<F,A> 
+//export function liftExt<F extends URIS,E>(x : Kind<F,E>) : <R>(cont : <X>(t : Kind<F,X>) => R) => Existential<F> {
+export function liftExt<F extends URIS4,A,B,C,E>(x : Kind4<F,A,B,C,E>) : Existential4<F,A,B,C> 
+export function liftExt<F extends URIS3,A,B,E>(x : Kind3<F,A,B,E>) : Existential3<F,A,B>
+export function liftExt<F extends URIS2,A,E>(x : Kind2<F,A,E>) : Existential2<F,A> 
+export function liftExt<F extends URIS,E>(x : Kind<F,E>) : Existential<F> {
+  return { runExt : (cont) => cont(x) }
 }
 
-const fooE_ = <A,E>(a : A, ex : E) : FooE<A,E> => (
-  { _tag : "FooE" 
-  , a 
-  , ex
-  }
-)
-
-declare module 'fp-ts/lib/HKT' {
-  interface URItoKind2<E,A> {
-    FooE: FooE<E,A>
-  }
-}
-
-const FooURI = "FooE"
-type FooURI = typeof FooURI
-
-type Foo<A> = Existential<FooURI,A>
-
-const foo = <A,E>(a : A, ex : E) : Foo<A> =>
-  liftExt<FooURI,A,E>(fooE_(a, ex))
-
-const foos : Foo<string>[] = 
-  [ foo("boo", 2)
-  , foo("bar", true)
-  ]
-
-
+//export function mapExt<F extends URIS4, A, B, C, AA, BB, CC>(f : <E>(a : Kind4<F,A, B, C, E>) => Kind4<F, AA, BB, CC, E>) : (x : Existential4<F, A, B, C>) => Existential4<F,AA, BB, CC> 
+//export function mapExt<F extends URIS3, A, B, AA, BB>(f : <E>(a : Kind3<F,A, B, E>) => Kind3<F,AA, BB, E>) : (x : Existential3<F,A,B>) => Existential3<F,AA, BB>
+//export function mapExt<F extends URIS2, A, B>(f : <E>(a : Kind2<F,A,E>) => Kind2<F,B,E>, x : Existential2<F,A>) : Existential2<F,B>
+//export function mapExt<F extends URIS>(f : <E>(a : Kind<F,E>) => Kind<F,E>, x : Existential<F>) : Existential<F> {
+//  return liftExt(x(f))
+//}
